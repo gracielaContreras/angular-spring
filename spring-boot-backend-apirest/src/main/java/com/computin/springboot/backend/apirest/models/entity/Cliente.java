59beff31f55de0f4d7aa8ad8a8235191c4b62318
graciela <gracielaestefani@gmail.com>
2020-08-02 03:26:35 -0400

    Implementado CRUD en backend API Rest

--- spring-boot-backend-apirest/src/main/java/com/computin/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/computin/springboot/backend/apirest/models/entity/Cliente.java
@@ -3,21 +3,14 @@ package com.computin.springboot.backend.apirest.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 @Entity
 @Table(name="cliente")
 public class Cliente implements Serializable{
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nombre;
@@ -27,7 +20,12 @@ public class Cliente implements Serializable{
 	private String email;
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
-	private Date createAt;	
+	private Date createAt;
+
+	@PrePersist
+	public void prePersist(){
+		createAt = new Date();
+	}
 	
 	public Long getId() {
 		return id;
--- spring-boot-backend-apirest/src/main/java/com/computin/springboot/backend/apirest/models/service/IClienteService.java
+++ spring-boot-backend-apirest/src/main/java/com/computin/springboot/backend/apirest/models/service/IClienteService.java
@@ -8,4 +8,10 @@ public interface IClienteService {
 	
 	public List<Cliente> findAll();
 
+	public Cliente save(Cliente cliente);
+
+	public Cliente findById(Long id);
+
+	public void delete(Long id);
+
 }
